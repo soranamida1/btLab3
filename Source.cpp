3825ed10464e184e1f3260c72d6c91f1f878f000
GiaThien <gle9882@gmail.com>
2025-11-01 11:21:07 +0700

    LeGiaThien

--- Source.cpp
+++ Source.cpp
@@ -8,12 +8,30 @@ float thuong(int a, int b);
 int main()
 {
 	int a; int b;
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Nhap so a";
-	cin>>a;
-	cout<<"Nhap so b";
-	cin>>b;
-	cout<<"Tong ="a+b;
+	cout << "DAY LA CHUONG TRINH DE thuc thi so hoan thien";
+	def is_perfect_number(n):
+    if n <= 1:
+        return False
+    sum_of_divisors = 1;
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            sum_of_divisors += i
+            if i != n // i: # Tránh cộng lặp lại nếu là số chính phương
+                sum_of_divisors += n // i
+    return sum_of_divisors == n
+
+# Ví dụ sử dụng
+int number = 6;
+if is_perfect_number(number):
+    print(f"{number} là số hoàn hảo")
+else:
+    print(f"{number} không phải là số hoàn hảo")
+
+int number = 28;
+if is_perfect_number(number):
+    print(f"{number} là số hoàn hảo")
+else:
+    print(f"{number} không phải là số hoàn hảo")
 	system("pause");
 	return 0;
 }
