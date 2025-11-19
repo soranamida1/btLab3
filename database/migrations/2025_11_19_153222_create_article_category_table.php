27a736c85f9012b8dc9174768c792ad31b07939c
mozzanhat <nguyenminhnhat211203@gmail.com>
2025-11-19 23:19:37 +0700

    Hoan thanh Tuan 1: Thiet lap Database va Seed du lieu

---
+++ database/migrations/2025_11_19_153222_create_article_category_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('article_category', function (Blueprint $table) {
+        $table->foreignId('article_id')->constrained()->onDelete('cascade');
+        $table->foreignId('category_id')->constrained()->onDelete('cascade');
+        // Tạo khóa chính kép để 1 bài không thuộc 1 danh mục 2 lần
+        $table->primary(['article_id', 'category_id']); 
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article_category');
+    }
+};
